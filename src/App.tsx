1c63c215cf2397d9bffd496d4e760923195c0ae4
abijith-831 <abhijithasokan831@gmail.com>
2025-11-27 23:08:35 +0530

    fsfs

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const App = () => {
       <SnackbarProvider maxSnack={3} autoHideDuration={3000} anchorOrigin={{vertical:'top',horizontal:'right'}}>
         <Routes>
           <Route   path='/' element={<ProtectedRoute> <MainPage /></ProtectedRoute>}/>
-          <Route.  path='/settings' element={<ProtectedRoute> <SettingsPage /></ProtectedRoute>}/>
+          <Route path='/settings' element={<ProtectedRoute> <SettingsPage /></ProtectedRoute>}/>
           <Route path='/login' element={<PublicRoute><Login /></PublicRoute>}/>
           <Route path='/signup' element={<PublicRoute><SignUp /></PublicRoute>}/>
 
