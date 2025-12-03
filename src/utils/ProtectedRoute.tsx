13582cb6d70a6b72c16bae4c20a3c0763a98cc0c
abijith-831 <abhijithasokan831@gmail.com>
2025-12-03 17:09:08 +0530

    public & protected route fixed

--- src/utils/ProtectedRoute.tsx
+++ src/utils/ProtectedRoute.tsx
@@ -1,8 +1,11 @@
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 import { RootState } from "../redux/store";
+import * as React from "react";
 
-export default function ProtectedRoute({ children }: { children: JSX.Element }) {
+
+
+export default function ProtectedRoute({ children }: { children: React.JSX.Element }) {
   const user = useSelector((state: RootState) => state.auth.currentUser);
 
   if (!user) {
--- src/utils/PublicRoute.tsx
+++ src/utils/PublicRoute.tsx
@@ -1,8 +1,9 @@
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 import { RootState } from "../redux/store";
+import * as React from "react";
 
-export default function PublicRoute({ children }: { children: JSX.Element }) {
+export default function PublicRoute({ children }: { children: React.JSX.Element }) {
   const user = useSelector((state: RootState) => state.auth.currentUser);
 
   if (user) {
